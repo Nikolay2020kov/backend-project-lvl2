797e77b65d60567e7d05b09f8b459b1e711abd1a
Nikolai Kovalev <nkovalev631@gmail.com>
2022-03-23 16:37:23 +0300

    yrok4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,7 @@
 import { test, expect } from '@jest/globals';
-import genDiff from '../src/index.js';
 import fs from 'fs';
 import path from 'path';
-
+import genDiff from '../src/index.js';
 
 const readFile = (filename) => fs.readFileSync(path.resolve(process.cwd(), filename.trim()), 'utf-8');
 
@@ -10,5 +9,5 @@ const result = readFile('/home/xehlet/backend-project-lvl2/__fixtures__/outputjs
 const x = '/home/xehlet/backend-project-lvl2/__fixtures__/file1.json';
 const y = '/home/xehlet/backend-project-lvl2/__fixtures__/file2.json';
 test('genDiff', () => {
-expect(genDiff(x,y)).toEqual(result);
+  expect(genDiff(x, y)).toEqual(result);
 });
--- src/index.js
+++ src/index.js
@@ -5,43 +5,41 @@ import _ from 'lodash';
 const readFile = (filename) => fs.readFileSync(path.resolve(process.cwd(), filename.trim()), 'utf-8');
 
 const genDiff = (filepath1, filepath2) => {
-  const result = new Array;
+  const result = [];
   const fileContent1 = readFile(filepath1);
   const fileContent2 = readFile(filepath2);
   const data1 = JSON.parse(fileContent1);
   const data2 = JSON.parse(fileContent2);
-result.push('{');
-  
+  result.push('{');
+
   const mass = { ...data1, ...data2 };
   const mass1 = _.sortBy(Object.keys(mass));
 
   for (const key of mass1) {
-    if (_.has(data1, key) === true && _.has(data2, key) === true) { 
+    if (_.has(data1, key) === true && _.has(data2, key) === true) {
       if (data1[key] === data2[key]) {
-	result.push(`    ${key}: ${data1[key]}`);
+        result.push(`    ${key}: ${data1[key]}`);
       }
-      if(data1[key] !== data2[key]){
+      if (data1[key] !== data2[key]) {
         result.push(`  - ${key}: ${data1[key]}`);
         result.push(`  + ${key}: ${data2[key]}`);
       }
     }
-    
+
     if (_.has(data1, key) === true && _.has(data2, key) === false) {
       result.push(`  - ${key}: ${data1[key]}`);
     }
     if (_.has(data1, key) === false && _.has(data2, key) === true) {
       result.push(`  + ${key}: ${data2[key]}`);
     }
-   }
-  
- result.push('}');
- result.push('');
+  }
 
-console.log (result.join('\n').toString());
+  result.push('}');
+  result.push('');
 
-return (result.join('\n').toString());
+  console.log(result.join('\n').toString());
+
+  return (result.join('\n').toString());
 };
-
-
 
 export default genDiff;
