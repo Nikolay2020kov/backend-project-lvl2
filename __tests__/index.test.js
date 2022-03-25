375c912e310404565190eaa8f55fc393cc9f9afd
Nikolai Kovalev <nkovalev631@gmail.com>
2022-03-25 13:02:49 +0300

    yrok4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,10 +4,10 @@ import path from 'path';
 import genDiff from '../src/index.js';
 
 const readFile = (filename) => fs.readFileSync(path.resolve(process.cwd(), filename.trim()), 'utf-8');
-
 const result = readFile('/home/xehlet/backend-project-lvl2/__fixtures__/outputjson.txt');
+const a = result.trim();
 const x = '/home/xehlet/backend-project-lvl2/__fixtures__/file1.json';
 const y = '/home/xehlet/backend-project-lvl2/__fixtures__/file2.json';
 test('genDiff', () => {
-  expect(genDiff(x, y)).toEqual(result);
+  expect(genDiff(x, y)).toEqual(a);
 });
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const genDiff = (filepath1, filepath2) => {
   const mass = { ...data1, ...data2 };
   const mass1 = _.sortBy(Object.keys(mass));
 
-  for (const key of mass1) {
+  mass1.forEach((key) => {
     if (_.has(data1, key) === true && _.has(data2, key) === true) {
       if (data1[key] === data2[key]) {
         result.push(`    ${key}: ${data1[key]}`);
@@ -32,10 +32,9 @@ const genDiff = (filepath1, filepath2) => {
     if (_.has(data1, key) === false && _.has(data2, key) === true) {
       result.push(`  + ${key}: ${data2[key]}`);
     }
-  }
+  });
 
   result.push('}');
-  result.push('');
 
   console.log(result.join('\n').toString());
 
