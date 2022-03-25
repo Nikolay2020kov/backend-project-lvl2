e1b2595bb57d269600070ec3dc0268820c6e3a16
Nikolai Kovalev <nkovalev631@gmail.com>
2022-03-25 15:56:06 +0300

    yrok4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,14 @@
+import { dirname, resolve } from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 import { test, expect } from '@jest/globals';
-import fs from 'fs';
-import path from 'path';
 import genDiff from '../src/index.js';
 
-const readFile = (filename) => fs.readFileSync(path.resolve(process.cwd(), filename.trim()), 'utf-8');
-const result = readFile('/home/xehlet/backend-project-lvl2/__fixtures__/outputjson.txt');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const result = readFile('../__fixtures__/outputjson.txt');
 const a = result.trim();
 const x = '/home/xehlet/backend-project-lvl2/__fixtures__/file1.json';
 const y = '/home/xehlet/backend-project-lvl2/__fixtures__/file2.json';
