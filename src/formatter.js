2215d46002d1b1ea42ec685996721318e1851161
Nikolay2020kov <nkovalev631@gmail.com>
2022-05-19 12:32:09 +0300

    york6

---
+++ src/formatter.js
@@ -0,0 +1,19 @@
+/*import _ from 'lodash';
+
+const keyOffset = 4;
+const prefixOffset = 2;
+const indentSymbol = ' ';
+const openSymbol = '{';
+const closeSymbol = '}';
+const labels = {
+ tree: ' ',
+ deleted: '-',
+added: '+',
+  unchanged: ' ',
+  nested: ' ',
+};*/
+
+const stylish = (tree) => {
+ console.log(tree);
+}
+export default stylish;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,12 @@
-import _ from 'lodash';
+import _ from "lodash";
 import parses from './parsers.js';
+import ast from './tree.js';
+import stylish from './formatter.js';
 
 const genDiff = (filepath1, filepath2) => {
-  const result = [];
   const data1 = parses(filepath1);
   const data2 = parses(filepath2);
-  const mass = { ...data1, ...data2 };
-  const mass1 = _.sortBy(Object.keys(mass));
-  result.push('{');
-  mass1.forEach((key) => {
-    if (_.has(data1, key) === true && _.has(data2, key) === true) {
-      if (data1[key] === data2[key]) {
-        result.push(`    ${key}: ${data1[key]}`);
-      }
-      if (data1[key] !== data2[key]) {
-        result.push(`  - ${key}: ${data1[key]}`);
-        result.push(`  + ${key}: ${data2[key]}`);
-      }
-    }
-
-    if (_.has(data1, key) === true && _.has(data2, key) === false) {
-      result.push(`  - ${key}: ${data1[key]}`);
-    }
-    if (_.has(data1, key) === false && _.has(data2, key) === true) {
-      result.push(`  + ${key}: ${data2[key]}`);
-    }
-  });
-
-  result.push('}');
-
-  console.log(result.join('\n').toString());
-
-  return (result.join('\n').toString());
+  const tree = ast(data1,data2); 
+  const format = stylish(tree);
 };
-
 export default genDiff;
