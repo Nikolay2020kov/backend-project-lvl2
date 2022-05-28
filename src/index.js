6296c095ce7e54435999beb64515daaa7bb8906b
Nikolay2020kov <nkovalev631@gmail.com>
2022-05-28 21:50:41 +0300

    yrok6

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
 import parses from './parsers.js';
 import ast from './tree.js';
 import stylish from './formatter.js';
@@ -6,7 +5,7 @@ import stylish from './formatter.js';
 const gendiff = (filepath1, filepath2) => {
   const data1 = parses(filepath1);
   const data2 = parses(filepath2);
-  const tree = ast(data1,data2); 
+  const tree = ast(data1, data2);
   const format = stylish(tree);
   console.log(format);
   return format;
--- src/tree.js
+++ src/tree.js
@@ -1,27 +1,29 @@
-#!/usr/bin/env node
-import _ from "lodash";
+import _ from 'lodash';
 
-const ast = (data1,data2) => {
-const mass = {...data1,...data2};
-const keys = Object.keys(mass);
-return keys.map((key) => {
- if(_.isPlainObject(data1[key]) === true && _.isPlainObject(data2[key]) === true) {
-   return {type: 'tree', key, children: ast(data1[key], data2[key])};
- }
- if (_.has(data1, key) === true && _.has(data2, key) === true) {
-    if (data1[key] === data2[key]) {
-      return {type: 'equal', key, value: data1[key]};
+const ast = (data1, data2) => {
+  const mass = { ...data1, ...data2 };
+  const keys = Object.keys(mass);
+  return keys.map((key) => {
+    if (_.isPlainObject(data1[key]) === true && _.isPlainObject(data2[key]) === true) {
+      return { type: 'tree', key, children: ast(data1[key], data2[key]) };
     }
-    if (data1[key] !== data2[key]) {
-      return {type: 'change', key, OldValue: data1[key],NewValue: data2[key]};
+    if (_.has(data1, key) === true && _.has(data2, key) === true) {
+      if (data1[key] === data2[key]) {
+        return { type: 'equal', key, value: data1[key] };
+      }
+      if (data1[key] !== data2[key]) {
+        return {
+          type: 'change', key, OldValue: data1[key], NewValue: data2[key],
+        };
+      }
     }
- }
- if (_.has(data1, key) === true && _.has(data2, key) === false) {
-   return {type: 'deleted', key, value: data1[key]};
- }
- if (_.has(data1, key) === false && _.has(data2, key) === true) {
-   return {type: 'added', key, value: data2[key]};
- }
-});
+    if (_.has(data1, key) === true && _.has(data2, key) === false) {
+      return { type: 'deleted', key, value: data1[key] };
+    }
+    if (_.has(data1, key) === false && _.has(data2, key) === true) {
+      return { type: 'added', key, value: data2[key] };
+    }
+    return 0;
+  });
 };
 export default ast;
