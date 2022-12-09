5e0239c4f9c250a1e0f744ce8aaefbe6deaae6ba
Nikolay2020kov <nkovalev631@gmail.com>
2022-12-09 21:07:03 +0300

    yrok 8

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -28,73 +28,73 @@ test('gendiff', () => {
 
 const g = readFile('../__fixtures__/file1.yaml');
 const k = readFile('../__fixtures__/file2.yaml');
-test('gendiff', () => {
+test('gendiff1', () => {
   expect(gendiff(g, k)).toEqual(a);
 });
 
 const l = readFile('../__fixtures__/file1.yml');
 const n = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff2', () => {
   expect(gendiff(l, n)).toEqual(a);
 });
 
 const m = readFile('../__fixtures__/file1.json');
 const o = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff3', () => {
   expect(gendiff(m, o)).toEqual(a);
 });
 
 const p = readFile('../__fixtures__/file1.yaml');
 const r = readFile('../__fixtures__/file2.json');
-test('gendiff', () => {
+test('gendiff4', () => {
   expect(gendiff(p, r)).toEqual(a);
 });
 
 const rp = readFile('../__fixtures__/file1.yaml');
 const pr = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff5', () => {
   expect(gendiff(rp, pr)).toEqual(a);
 });
 
 const ca = readFile('../__fixtures__/file1.json');
 const db = readFile('../__fixtures__/file2.json');
-test('gendiff', () => {
+test('gendiff6', () => {
   expect(gendiff(ca, db, 'stylish')).toEqual(a);
 });
 
 const ag = readFile('../__fixtures__/file1.yaml');
 const bk = readFile('../__fixtures__/file2.yaml');
-test('gendiff', () => {
+test('gendiff7', () => {
   expect(gendiff(ag, bk, 'stylish')).toEqual(a);
 });
 
 const lc = readFile('../__fixtures__/file1.yml');
 const nd = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff8', () => {
   expect(gendiff(lc, nd, 'stylish')).toEqual(a);
 });
 
 const am = readFile('../__fixtures__/file1.json');
 const bo = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff9', () => {
   expect(gendiff(am, bo, 'stylish')).toEqual(a);
 });
 
 const bp = readFile('../__fixtures__/file1.yaml');
 const ra = readFile('../__fixtures__/file2.json');
-test('gendiff', () => {
+test('gendiff10', () => {
   expect(gendiff(bp, ra, 'stylish')).toEqual(a);
 });
 
 const pb = readFile('../__fixtures__/file1.yaml');
 const ar = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff11', () => {
   expect(gendiff(pb, ar, 'stylish')).toEqual(a);
 });
 
 const zxc = readFile('../__fixtures__/file1.json');
 const cxz = readFile('../__fixtures__/file2.json');
-test('gendiff', () => {
+test('gendiff12', () => {
   expect(gendiff(zxc, cxz, 'styl')).toEqual(yy);
 });
 
@@ -102,37 +102,37 @@ const res = readFileSync(readFile('../__fixtures__/OutputPlain.txt'), 'utf-8');
 const b = res.trim();
 const e = readFile('../__fixtures__/file1.json');
 const f = readFile('../__fixtures__/file2.json');
-test('gendiff', () => {
+test('gendiff13', () => {
   expect(gendiff(e, f, 'plain')).toEqual(b);
 });
 
 const h = readFile('../__fixtures__/file1.yaml');
 const j = readFile('../__fixtures__/file2.yaml');
-test('gendiff', () => {
+test('gendiff14', () => {
   expect(gendiff(h, j, 'plain')).toEqual(b);
 });
 
 const z = readFile('../__fixtures__/file1.yml');
 const x = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff15', () => {
   expect(gendiff(z, x, 'plain')).toEqual(b);
 });
 
 const q = readFile('../__fixtures__/file1.json');
 const w = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff16', () => {
   expect(gendiff(q, w, 'plain')).toEqual(b);
 });
 
 const v = readFile('../__fixtures__/file1.yaml');
 const i = readFile('../__fixtures__/file2.json');
-test('gendiff', () => {
+test('gendiff17', () => {
   expect(gendiff(v, i, 'plain')).toEqual(b);
 });
 
 const iv = readFile('../__fixtures__/file1.yaml');
 const vi = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff18', () => {
   expect(gendiff(iv, vi, 'plain')).toEqual(b);
 });
 
@@ -140,36 +140,36 @@ const resa = readFileSync(readFile('../__fixtures__/OutputJson.txt'), 'utf-8');
 const ab = resa.trim();
 const eg = readFile('../__fixtures__/file1.json');
 const fg = readFile('../__fixtures__/file2.json');
-test('gendiff', () => {
+test('gendiff19', () => {
   expect(gendiff(eg, fg, 'json')).toEqual(ab);
 });
 
 const he = readFile('../__fixtures__/file1.yaml');
 const je = readFile('../__fixtures__/file2.yaml');
-test('gendiff', () => {
+test('gendiff20', () => {
   expect(gendiff(he, je, 'json')).toEqual(ab);
 });
 
 const za = readFile('../__fixtures__/file1.yml');
 const xa = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff21', () => {
   expect(gendiff(za, xa, 'json')).toEqual(ab);
 });
 
 const qy = readFile('../__fixtures__/file1.json');
 const wy = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff22', () => {
   expect(gendiff(qy, wy, 'json')).toEqual(ab);
 });
 
 const av = readFile('../__fixtures__/file1.yaml');
 const ai = readFile('../__fixtures__/file2.json');
-test('gendiff', () => {
+test('gendiff23', () => {
   expect(gendiff(av, ai, 'json')).toEqual(ab);
 });
 
 const va = readFile('../__fixtures__/file1.yaml');
 const ia = readFile('../__fixtures__/file2.yml');
-test('gendiff', () => {
+test('gendiff24', () => {
   expect(gendiff(va, ia, 'json')).toEqual(ab);
 });
